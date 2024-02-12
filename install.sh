4e43fba21c6cf0b7ea769428bebe9e126d646a58
freitaseric <freitaseric@proton.me>
2024-02-12 13:28:11 -0400

    feat: adding install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo
+echo Eric.Files
+echo Instalador
+echo
+echo
+echo
+echo
+echo Atualizando as configurações do PowerLevel10K...
+
+cp ./.p10k.zsh ~/.p10k.zsh
+
+echo Configurações do PowerLevel10K atualizadas!
+echo
+echo A instalação chegou ao fim!
+echo Obrigado por usar o Eric.Files!
